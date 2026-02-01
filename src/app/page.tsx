823ae18f0f149ce1ea3ce3405dbaa0d5f02edb3c
Erdy Yudanto <erdyyudanto88@gmail.com>
2026-02-01 08:22:17 +0000

    Perbaiki layoutnya, pastikan jarak margin da]]kanan kiri sesuai

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-dvh">
       <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <div className="container flex h-14 max-w-screen-2xl items-center justify-between">
+        <div className="container flex h-14 items-center justify-between">
           <a href="#" className="flex items-center gap-2">
             <DogPawIcon className="w-8 h-8 text-primary" />
             <span className="font-headline text-2xl font-bold text-foreground">
